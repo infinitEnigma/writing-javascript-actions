23afd08532919cda10a1a84ea2a635a1421d86f2
infinitEnigma <yole71@gmail.com>
2020-04-05 14:37:35 +0200

    delete node_modules

---
+++ .github/actions/issue-maker/dist/index.js
@@ -0,0 +1,72 @@
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	__webpack_require__.ab = __dirname + "/";
+/******/
+/******/ 	// the startup function
+/******/ 	function startup() {
+/******/ 		// Load entry module and return exports
+/******/ 		return __webpack_require__(496);
+/******/ 	};
+/******/
+/******/ 	// run startup
+/******/ 	return startup();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 496:
+/***/ (function() {
+
+async function run() {
+  try {
+    const issueTitle = core.getInput("issue-title");
+    const jokeBody = core.getInput("joke");
+    const token = core.getInput("repo-token");
+
+    const octokit = new github.Github(token);
+
+    const newIssue = await octokit.issues.create({
+        repo: github.context.repo.repo,
+        owner: github.context.repo.owner,
+        title: issueTitle,
+        body: jokeBody
+    });
+  } catch (err) {
+      core.setFailed(error.message);
+  }
+}
+
+run()
+
+/***/ })
+
+/******/ });
